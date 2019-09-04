346a5347be4aa95196e53f7f76fac1d656c63b45
MeulemN <nick.meulemeester@basf.com>
2019-09-04 11:41:41 +0200

    orcid data

--- pull_experience.R
+++ pull_experience.R
@@ -1,3 +1,6 @@
+library(rorcid)
+library(purrr)
+library(dplyr)
 # only run once to get token
 # rorcid::orcid_auth() 
 # copy token to .Renviron
@@ -5,16 +8,19 @@
 
 orcidID <- "0000-0001-7938-7188"
 
-#empl <- rorcid::orcid_employments(orcidID)$`0000-0001-7938-7188`$`affiliation-group`$summaries
+empl <- rorcid::orcid_employments(orcidID)$`0000-0001-7938-7188`$`affiliation-group`$summaries
 #empl <- map(empl, ~ as.character(.x$'employment-summary.role-title'))
-# empl <- reduce(
-#   empl,
-#   full_join
-#   )
+ empl <- reduce(
+   empl,
+   full_join
+   )
+empl$when <- glue::glue("{empl$`employment-summary.start-date.year.value`} - {empl$`employment-summary.end-date.year.value`}")
 write.csv(empl, "data/empl.csv", row.names = FALSE)
 
-#edu <- reduce(
-  #   rorcid::orcid_educations(orcidID)$`0000-0001-7938-7188`$`affiliation-group`$summaries,
-  #   full_join
-  #   )
+
+edu <- reduce(
+   rorcid::orcid_educations(orcidID)$`0000-0001-7938-7188`$`affiliation-group`$summaries,
+   full_join
+   )
+edu$when <- glue::glue("{edu$`education-summary.start-date.year.value`} - {edu$`education-summary.end-date.year.value`}")
 write.csv(edu, "data/edu.csv", row.names = FALSE)
